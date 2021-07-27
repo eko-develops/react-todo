50dd7dfff9714410fec2081abfe649d9a3751b17
eko-develops <ekodevelops@gmail.com>
2021-07-27 11:36:51 -0700

    Create starter server with express

---
+++ server.js
@@ -0,0 +1,15 @@
+/*For setting up the server and calling the database connection */
+
+require('dotenv').config()  //we want to require and configure dotenv as early as possible in our application
+
+const express = require('express') //web framework for nodejs
+const app = express()
+const PORT = process.env.PORT   //setting the port
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(PORT, () => {
+  console.log(`listening at http://localhost:${PORT}`)
+})
